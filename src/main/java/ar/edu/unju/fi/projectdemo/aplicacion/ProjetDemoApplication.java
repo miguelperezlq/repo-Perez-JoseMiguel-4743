d6e232e77db5fd5eccd938067b7847b5d0ebf8c2
Miguel Perez <Miguel Perez@Miguel>
2022-04-10 18:01:45 -0300

    Primer commit

---
+++ src/main/java/ar/edu/unju/fi/projectdemo/aplicacion/ProjetDemoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.projectdemo.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/fi/projectdemo/aplicacion/ProjetDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.projectdemo.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
